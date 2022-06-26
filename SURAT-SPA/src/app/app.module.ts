e3d2b875362ac9fe823f5526d2e7da0f2038c555
wap98 <wap@jiwalu.id>
2022-06-26 07:00:25 +0700

    update ui

--- SURAT-SPA/src/app/app.module.ts
+++ SURAT-SPA/src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SuratmasukComponent } from './suratmasuk/suratmasuk.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { ToastrModule } from 'ngx-toastr';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+      AppComponent,
+      HomeComponent,
+      LoginComponent,
+      SuratmasukComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ToastrModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TableModule,
+    ToastrModule.forRoot({ 
+      positionClass: 'toast-bottom-right',
+      maxOpened: 2,
+      autoDismiss: true
+     }),
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ SURAT-SPA/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() {
+    document.body.style.backgroundImage = "url('assets/bg-home.png')";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundSize = "cover";
+   }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy(){
+    document.body.style.backgroundImage = "none";
+  }
+
+}
